b500eb5e440f1d1b2911365df35335772d571efd
KalabSisaySE <kyimusic11@gmail.com>
2023-08-28 16:22:28 +0300

    fix task-0

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,5 +1,11 @@
 import $ from "jquery";
 
-$("body").append(`<p> Holberton Dashboard </p>`);
-$("body").append(`<p> Dashboard data for the students </p>`);
-$("body").append(`<p> Copyright - Holberton School </p>`);
+const paragraphs = [
+  "Holberton Dashboard",
+  "Dashboard data for the students",
+  "Copyright - Holberton School",
+];
+
+for (let paragraph of paragraphs) {
+  $("body").append(`<p>${paragraph}</p>`);
+}
