5ac3df873ae76f069e5a8154843ce7cab042186e
KalabSisaySE <kyimusic11@gmail.com>
2023-08-30 14:03:30 +0300

    fix task-1

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,21 +1,20 @@
-import $ from 'jquery';
-import _ from 'lodash';
+const $ = require('jquery');
+const _ = require('lodash');
 
 let count = 0;
 
-const updateCounter = () => {
-  const p = $('#count');
-  count += 1;
-  p.text(`${count} clicks on the button`);
-};
+function updateCounter() {
+  count++;
+  $('#count').text(`${count} clicks on the button`);
+}
 
-const btn = $('<button>Click here to get started</button>').on(
+const $button = $('<button>Click here to get started</button>').on(
   'click',
-  _.debounce(updateCounter, 100),
+  _.debounce(updateCounter, 500, { leading: true, trailing: false }),
 );
 
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
-$('body').append(btn);
-$('body').append('<p id="count"></p>');
+$('body').append($button);
+$('body').append("<p id='count'></p>");
 $('body').append('<p>Copyright - Holberton School</p>');
