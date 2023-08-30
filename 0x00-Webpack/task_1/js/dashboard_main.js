f04388616978371076839da2bbaee716fdf1dc49
KalabSisaySE <kyimusic11@gmail.com>
2023-08-30 15:06:45 +0300

    test-checker: package.json

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -7,15 +7,17 @@ $('body').append('<button>Click here to get started</button>');
 $('body').append('<p id="count"></p>');
 $('body').append('<p>Copyright - Holberton School</p>');
 
-let count = 0;
-
 function updateCounter() {
-  count++;
+  const p = $('#count');
+  const text = p.text();
+  if (text.length === 0) {
+    p.text('1 clicks on the button');
+  } else {
+    let count = Number(text.split(' ')[0]);
+    count += 1;
+    p.text(`${count} clicks on the button`);
+  }
 }
 
-$('button').click(
-  _.debounce(() => {
-    updateCounter();
-    $('#count').text(`${count} clicks on the button`);
-  }),
-);
+const btn = $(':button');
+$(btn).on('click', _.debounce(updateCounter, 50));
