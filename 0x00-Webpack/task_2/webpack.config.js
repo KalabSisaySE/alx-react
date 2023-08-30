5d765c5eabef855a5ae83abf6151864405c9cd12
KalabSisaySE <kyimusic11@gmail.com>
2023-08-30 16:28:06 +0300

    fix task-2

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -2,6 +2,19 @@ const path = require('path');
 
 module.exports = {
   entry: path.resolve(__dirname, 'js/dashboard_main.js'),
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  performance: { maxAssetSize: 500000 },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public'),
